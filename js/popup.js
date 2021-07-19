e9c8ad9f3c73614eea5d2e699a366cb67005b77c
rakitinavi <v.rakitina@krasniykarandash.ru>
2021-07-19 20:03:59 +0300

    исправление ошибок на защиту. 2

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 import {isEscEvent, isEnterEvent} from './utils.js';
 import {PopupType} from './constants.js';
 
@@ -8,22 +9,30 @@ const popupTemplate = {
 
 let activePopup = null;
 
+const onPopupKeyDown = (evt) => {
+  if (isEscEvent(evt) || isEnterEvent(evt)) {
+    evt.preventDefault();
+    removePopup();
+  }
+};
+
 const removePopup = () => {
   if (activePopup !== null) {
     activePopup.remove();
     document.removeEventListener('keydown', onPopupKeyDown);
     activePopup = null;
   }
-}
+};
 
-const onPopupKeyDown = (evt) => {
-  if (isEscEvent(evt) || isEnterEvent(evt)) {
-    evt.preventDefault();
-    removePopup();
-  }
-}
 
 const onPopupClick = () => removePopup();
+
+const showPopup = (popupType) => {
+  activePopup = popupTemplate[popupType].cloneNode(true);
+  document.body.insertAdjacentElement('afterbegin', activePopup);
+  activePopup.addEventListener('click', onPopupClick);
+  document.addEventListener('keydown', onPopupKeyDown);
+};
 
 
 export {showPopup};
